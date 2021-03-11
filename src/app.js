65b9040916662b7767f23776ef09c5959a06e4db
Abdullah H <its.dev.abdullah@gmail.com>
2021-03-11 09:26:15 +0300

    added customizable css vars, tweaked js code

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 var signatureCreator = {
-    makeSignatureEditor: makeSignatureEditor,
-    impl: {
-        options: {}
-    }
+  makeSignatureEditor: makeSignatureEditor,
+  impl: {
+    options: {},
+  },
 };
 
 // Default impl options
 var defaultOptions = {
-    dir: 'ltr',
-    saveBtnLabel: 'Save As Image',
-    editBtnLabel: 'Edit Texts',
-    previewBtnLabel: 'Preview',
-    fileName: 'signature'
+  dir: "ltr",
+  saveBtnLabel: "Save As Image",
+  editBtnLabel: "Edit Texts",
+  previewBtnLabel: "Preview",
+  fileName: "signature",
 };
 
 /**
@@ -19,49 +19,39 @@ var defaultOptions = {
  * @param {Object} options - Rendering options
  * */
 function makeSignatureEditor(node, options) {
-    options = options || {};
-    copyOptions(options);
-    options = signatureCreator.impl.options;
-    
-    Promise.resolve(node)
-        .then(function (node) {
-            buildHTMLStructure(node);
-        })
-        .then(function () {
-            bindFunctions(node);
-        })
-        .then(function () {
-            fittingZoom(node);
-        });
+  options = options || {};
+  copyOptions(options);
+  options = signatureCreator.impl.options;
+
+  buildHTMLStructure(node);
+  bindFunctions(node);
+  fittingZoom(node);
 }
 
 function copyOptions(options) {
-    for (var key of Object.keys(defaultOptions)) {
-        if (options[key] == undefined)
-            signatureCreator.impl.options[key] = defaultOptions[key];
-        else
-            signatureCreator.impl.options[key] = options[key];
-    }
+  for (var key of Object.keys(defaultOptions)) {
+    if (options[key] == undefined)
+      signatureCreator.impl.options[key] = defaultOptions[key];
+    else signatureCreator.impl.options[key] = options[key];
+  }
 }
 
 function buildHTMLStructure(node) {
-    node.classList.add('signature-container');
+  node.classList.add("signature-container");
 
-    if (signatureCreator.impl.options.dir === "rtl")
-        node.setAttribute('dir', signatureCreator.impl.options.dir);
+  if (signatureCreator.impl.options.dir === "rtl")
+    node.setAttribute("dir", signatureCreator.impl.options.dir);
 
-    var userHTML = node.innerHTML;
-    var signatureHTML = `<div class="signature">${userHTML}</div>`;
+  var userHTML = node.innerHTML;
+  var signatureHTML = `<div class="signature">${userHTML}</div>`;
 
-    node.innerHTML = signatureHTML;
+  node.innerHTML = signatureHTML;
 
-    bindControls(node);
-
-    return true;
+  bindControls(node);
 }
 
 function bindControls(node) {
-    var html = `<div class="control-buttons preview-mode">
+  var html = `<div class="control-buttons preview-mode">
                     <button class="btn btn-primary save" type="button">
                     ${signatureCreator.impl.options.saveBtnLabel}
                     </button>
@@ -73,173 +63,186 @@ function bindControls(node) {
                     </button>
                 </div>`;
 
-    node.innerHTML += html;
+  node.innerHTML += html;
 }
 
 function bindFunctions(node) {
-    node.querySelector('.control-buttons > .save').addEventListener('click', function () {
-        download(node);
+  node
+    .querySelector(".control-buttons > .save")
+    .addEventListener("click", function () {
+      download(node);
     });
 
-    node.querySelector('.control-buttons > .edit').addEventListener('click', function () {
-        edit(node);
+  node
+    .querySelector(".control-buttons > .edit")
+    .addEventListener("click", function () {
+      edit(node);
     });
 
-    node.querySelector('.control-buttons > .preview').addEventListener('click', function () {
-        preview(node);
+  node
+    .querySelector(".control-buttons > .preview")
+    .addEventListener("click", function () {
+      preview(node);
     });
 
-    node.querySelectorAll('.signature p').forEach(function (e) {
-        e.addEventListener('focus', function () {
-            selectText(e);
-        });
+  node.querySelectorAll(".signature p").forEach(function (e) {
+    e.addEventListener("focus", function () {
+      selectText(e);
     });
+  });
 }
 
 function download(node) {
-    node.querySelector(".control-buttons").classList.add("hide");
-    document.querySelector("body").style.overflow = "hidden";
+  node.querySelector(".control-buttons").classList.add("hide");
+  document.querySelector("body").classList.add("no-overflow");
 
-    var currentView = node.querySelector(".control-buttons").classList.contains('edit-mode') ? "EditMode" : "PreviewMode";
+  var currentView = node
+    .querySelector(".control-buttons")
+    .classList.contains("edit-mode")
+    ? "EditMode"
+    : "PreviewMode";
 
-    originalZoom(node, false);
+  originalZoom(node, false);
 
-    toggleContentEditable(node, false);
+  toggleContentEditable(node, false);
 
-    clearSelection();
+  clearSelection();
 
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 
-    node.querySelectorAll('.signature p').forEach(function (e) {
-        e.innerHTML = e.innerHTML.replace(/ /g, "&nbsp;");
-    });
+  node.querySelectorAll(".signature p").forEach(function (e) {
+    e.innerHTML = e.innerHTML.replace(/ /g, "&nbsp;");
+  });
 
-    html2canvas(node.querySelector('.signature')).then(function (canvas) {
-        var link = document.createElement("a");
-        document.body.appendChild(link);
-        link.download = signatureCreator.impl.options.fileName + ".jpg";
-        link.href = canvas.toDataURL("image/jpeg");
-        link.target = "_blank";
-        link.click();
-    });
+  html2canvas(node.querySelector(".signature")).then(function (canvas) {
+    var link = document.createElement("a");
+    document.body.appendChild(link);
+    link.download = signatureCreator.impl.options.fileName + ".jpg";
+    link.href = canvas.toDataURL("image/jpeg");
+    link.target = "_blank";
+    link.click();
+  });
 
-    node.parentElement.querySelector(".control-buttons").classList.remove("hide");
-    document.querySelector("body").style.overflow = null;
+  node.parentElement.querySelector(".control-buttons").classList.remove("hide");
+  document.querySelector("body").classList.remove("no-overflow");
 
-    if (currentView == "EditMode")
-        edit(node, false);
-    else
-        preview(node, false);
+  if (currentView == "EditMode") edit(node, false);
+  else preview(node, false);
 }
 
 function preview(node, animation) {
-    node.querySelector(".control-buttons").classList.add("preview-mode");
-    node.querySelector(".control-buttons").classList.remove("edit-mode");
+  node.querySelector(".control-buttons").classList.add("preview-mode");
+  node.querySelector(".control-buttons").classList.remove("edit-mode");
 
-    toggleContentEditable(node, false);
+  toggleContentEditable(node, false);
 
-    clearSelection();
+  clearSelection();
 
-    fittingZoom(node, animation);
+  fittingZoom(node, animation);
 
-    // window.scrollTo(0, 0);
+  // window.scrollTo(0, 0);
 }
 
 function edit(node, animation) {
-    //to be used later with input focusing
-    var sideOffset =
-    node.querySelector('.signature p').offsetLeft +
-    (signatureCreator.impl.options.dir == 'rtl'? node.querySelector('.signature p').scrollWidth + 10 : -10);
-    sideOffset = sideOffset > window.innerWidth ? sideOffset - window.innerWidth : sideOffset;
+  //to be used later with input focusing
+  var sideOffset =
+    node.querySelector(".signature p").offsetLeft +
+    (signatureCreator.impl.options.dir == "rtl"
+      ? node.querySelector(".signature p").scrollWidth + 10
+      : -10);
+  sideOffset =
+    sideOffset > window.innerWidth
+      ? sideOffset - window.innerWidth
+      : sideOffset;
 
-    originalZoom(node, animation);
+  originalZoom(node, animation);
 
-    node.querySelector(".control-buttons").classList.add("edit-mode");
-    node.querySelector(".control-buttons").classList.remove("preview-mode");
+  node.querySelector(".control-buttons").classList.add("edit-mode");
+  node.querySelector(".control-buttons").classList.remove("preview-mode");
 
-    toggleContentEditable(node, true);
+  toggleContentEditable(node, true);
 
-    node.querySelector('.signature p').focus();
-    node.querySelector('.signature p').scrollIntoView()
+  node.querySelector(".signature p").focus();
+  node.querySelector(".signature p").scrollIntoView();
 
-    window.scrollTo(sideOffset, window.scrollY, 'smooth');
+  window.scrollTo(sideOffset, window.scrollY, "smooth");
 
-    setTimeout(function () {
-        node.querySelector('.signature p').focus();
-    node.querySelector('.signature p').scrollIntoView()
-        window.scrollTo(sideOffset, window.scrollY, 'smooth');
-    }, 200);
+  setTimeout(function () {
+    node.querySelector(".signature p").focus();
+    node.querySelector(".signature p").scrollIntoView();
+    window.scrollTo(sideOffset, window.scrollY, "smooth");
+  }, 200);
 }
 
 function fittingZoom(node, animation = true) {
-    if (animation)
-        node.querySelector('.signature').classList.remove('no-animation');
-    else
-        node.querySelector('.signature').classList.add('no-animation');
+  if (animation)
+    node.querySelector(".signature").classList.remove("no-animation");
+  else node.querySelector(".signature").classList.add("no-animation");
 
-    node.style.width = null;
-    if (node.clientWidth == node.scrollWidth) return;
+  node.style.width = null;
+  if (node.clientWidth == node.scrollWidth) return;
 
-    document.querySelector("body").style.overflow = "hidden";
+  document.querySelector("body").style.overflow = "hidden";
 
-    var fittingZoomNumb =
-        (node.clientWidth * 1) / node.scrollWidth;
+  var fittingZoomNumb = (node.clientWidth) / node.scrollWidth;
 
-    node.querySelector('.signature').style.transform =
-        "scale(" + fittingZoomNumb + ")";
+  node.querySelector('.signature').style.transform =
+      "scale(" + fittingZoomNumb + ")";
 
-    document.querySelector("body").style.overflow = null;
+  document.querySelector("body").style.overflow = null;
 
-    setTimeout(function () {
-        window.scrollTo(0, 0, 'smooth');
-    }, 0);
+  setTimeout(function () {
+    window.scrollTo(0, 0, "smooth");
+  }, 0);
 }
 
 function originalZoom(node, animation = true) {
-    if (animation)
-        node.querySelector('.signature').classList.remove('no-animation');
-    else
-        node.querySelector('.signature').classList.add('no-animation');
+  if (animation)
+    node.querySelector(".signature").classList.remove("no-animation");
+  else node.querySelector(".signature").classList.add("no-animation");
 
-    node.querySelector('.signature').style.transform = null;
+  node.querySelector(".signature").style.transform = null;
 
-    node.style.width = node.querySelector('.signature').scrollWidth + 'px';
+  node.style.width = node.querySelector(".signature").scrollWidth + "px";
 }
 
 function toggleContentEditable(node, toggleState) {
-    node.querySelectorAll('.signature p').forEach(function (e) {
-        e.setAttribute("contenteditable", toggleState);
-    });
+  node.querySelectorAll(".signature p").forEach(function (e) {
+    e.setAttribute("contenteditable", toggleState);
+  });
 }
 
 // Utilities
 function selectText(node) {
-    if (document.body.createTextRange) {
-        const range = document.body.createTextRange();
-        range.moveToElementText(node);
-        range.select();
-    } else if (window.getSelection) {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNodeContents(node);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    } else {
-        console.warn("Could not select text in node: Unsupported browser.");
-    }
+  if (document.body.createTextRange) {
+    const range = document.body.createTextRange();
+    range.moveToElementText(node);
+    range.select();
+  } else if (window.getSelection) {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  } else {
+    console.warn("Could not select text in node: Unsupported browser.");
+  }
 }
 
 function clearSelection() {
-    if (window.getSelection) { window.getSelection().removeAllRanges(); }
-    else if (document.selection) { document.selection.empty(); }
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+  } else if (document.selection) {
+    document.selection.empty();
+  }
 }
 
 function createElementFromHTML(htmlString) {
-    var div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
+  var div = document.createElement("div");
+  div.innerHTML = htmlString.trim();
 
-    // Change this to div.childNodes to support multiple top-level nodes
-    return div.firstChild;
+  // Change this to div.childNodes to support multiple top-level nodes
+  return div.firstChild;
 }
 
 export default signatureCreator;
